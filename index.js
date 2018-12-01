4de8c047d9756c5e93c6ce5eaf92b8c83884f749
McVenco <mcvenco@gmail.com>
2018-12-01 21:28:15 +0100

    Some final cleanup

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 module.exports = (num) => {
     if (typeof num !== 'number') {
-        throw new TypeError('klingnum needs a number as input!')
-    } else if(num >= 10000000 && num < 0) {
-        throw new Error('klingnum needs a number between 0 and 9.999.999')
+        throw new TypeError('klingNum needs a number as input!');
+    } else if (num >= 10000000 || num < 0) {
+        throw new Error('klingNum needs a number between 0 and 9.999.999');
+    } 
+
+    let string = num.toString(),
+        numArray = num.toString().split(''),
+        numbers, multipliers, klingText;
+
+    // numbers 0-9
+    numbers = ["pagh", "wa'", "cha'", "wej", "loS", "vagh", "jav", "Soch", "chorgh", "Hut"];
+
+    // multipliers 0, 10, 100 ... 1000000 (one million)
+    multipliers = ["", "maH", "vatlh", "SaD", "netlh", "bIp", "'uy'"];
+
+    klingText = [];
+
+    // is num zero?
+    if (parseInt(string) === 0){
+        return numbers[0];
     }
 
-    const numbers = ["pagh", "wa'", "cha'", "wej", "loS", "vagh", "jav", "Soch", "chorgh", "Hut"];
-    const multipliers = ["", "maH", "vatlh", "SaD", "netlh", "bIp", "'uy'"];
+    for (var i = 0; i < numArray.length; i++) {
+        klingText.push(numbers[numArray[i]] + multipliers[numArray.length - 1 - i]);
+    }
 
-    var numArray = num.toString().split('');
-    console.log(numArray);
+    return klingText.join(' ');
 }
--- test.js
+++ test.js
@@ -3,21 +3,21 @@
 function klingNum(num){
     if (typeof num !== 'number') {
         throw new TypeError('klingNum needs a number as input!');
-    } else if ((num >= 10000000) || (num < 0)) {
+    } else if (num >= 10000000 || num < 0) {
         throw new Error('klingNum needs a number between 0 and 9.999.999');
     } 
 
     let string = num.toString(),
         numArray = num.toString().split(''),
-        numbers, multipliers, chunk;
+        numbers, multipliers, klingText;
 
     // numbers 0-9
     numbers = ["pagh", "wa'", "cha'", "wej", "loS", "vagh", "jav", "Soch", "chorgh", "Hut"];
 
-    // multipliers 0, 10, 100 --- 1000000 (one million)
+    // multipliers 0, 10, 100 ... 1000000 (one million)
     multipliers = ["", "maH", "vatlh", "SaD", "netlh", "bIp", "'uy'"];
 
-    chunk = [];
+    klingText = [];
 
     // is num zero?
     if (parseInt(string) === 0){
@@ -25,10 +25,10 @@ function klingNum(num){
     }
 
     for (var i = 0; i < numArray.length; i++) {
-        chunk.push(numbers[numArray[i]] + multipliers[numArray.length - 1 - i]);
+        klingText.push(numbers[numArray[i]] + multipliers[numArray.length - 1 - i]);
     }
 
-    return chunk.join(' ');
+    return klingText.join(' ');
 };
 
 console.log(klingNum(0));
