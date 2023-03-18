04e5e1cbe6e7685a5ff2bbd2c52e5d9a7dbbcd06
Eric Vonk <mcvenco@gmail.com>
2023-03-18 20:44:16 +0100

    Rewrite and optimize

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 'use strict';
 
 module.exports = (num) => {
-    if (typeof num !== 'number') {
+    if (typeof num !== 'number' || typeof num === "undefined") {
         throw new TypeError('klingNum needs a number as input!');
     } else if (num >= 10000000 || num < 0) {
-        throw new Error('klingNum needs a number between 0 and 9.999.999');
-    } 
-
-    let string = num.toString(),
-        numArray = num.toString().split(''),
-        numbers, multipliers, klingText;
-
+        throw new Error('klingNum can only handle numbers between 0 and 9,999,999');
+    }
+      
     // numbers 0-9
-    numbers = ["pagh", "wa'", "cha'", "wej", "loS", "vagh", "jav", "Soch", "chorgh", "Hut"];
-
+    const numbers = ["pagh", "wa'", "cha'", "wej", "loS", "vagh", "jav", "Soch", "chorgh", "Hut"];    
     // multipliers 0, 10, 100 ... 1000000 (one million)
-    multipliers = ["", "maH", "vatlh", "SaD", "netlh", "bIp", "'uy'"];
+    const multipliers = ["", "maH", "vatlh", "SaD", "netlh", "bIp", "'uy'"];
 
-    klingText = [];
+    let klingText = [];
+    
+    let numArray = [...num.toString()].map(Number);
 
-    // is num zero?
-    if (parseInt(string) === 0){
-        return numbers[0];
-    }
+    numArray.forEach((digit, index) => {
+        klingText.push(numbers[digit] + multipliers[numArray.length - 1 - index]);
+    });
 
-    for (let i = 0; i < numArray.length; i++) {
-        klingText.push(numbers[numArray[i]] + multipliers[numArray.length - 1 - i]);
-    }
-
-    return klingText.join(' ');
+    // if num is zero, return 'pagh', otherwise return klingText
+    return num === 0 ? numbers[0] : klingText.join(' ');
 }
